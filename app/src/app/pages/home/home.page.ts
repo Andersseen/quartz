a22f6a2b68672983fb9fd1ce34dc479a799ca3a2
andersseen <andriipap01@gmail.com>
2026-04-03 22:58:44 +0200

    refactor: extract header into a standalone component and update layout styling

--- app/src/app/pages/home/home.page.ts
+++ app/src/app/pages/home/home.page.ts
@@ -1,9 +1,10 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { RouterLink } from '@angular/router';
+import { HeaderComponent } from '../../components/header/header.component';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, HeaderComponent],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './home.page.html',
   styleUrl: './home.page.scss',
