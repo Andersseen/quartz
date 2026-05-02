1c5bcb3b17f062539d375a8c58b788bdf0601bf0
andersseen <andriipap01@gmail.com>
2026-05-02 16:26:47 +0200

    setup tailwind

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
+import './tailwind.css';
+import './styles.scss';
 import './app/demo-styles.scss';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { App } from './app/app.component';
 import { appConfig } from './app/app.config';
-import './styles.scss';
 
 bootstrapApplication(App, appConfig).catch((err) => console.error(err));
